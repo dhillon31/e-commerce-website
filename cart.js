2cc666cf4975cd477430b40b7cc8c233a9fedb39
dhillon31 <lovepreetdhillon3694@gmail.com>
2026-06-27 07:05:15 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,69 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const cartItems = document.getElementById("cartItems");
+const totalPrice = document.getElementById("totalPrice");
+
+function displayCart(){
+
+cartItems.innerHTML = "";
+
+let total = 0;
+
+if(cart.length === 0){
+cartItems.innerHTML = "<h2>Cart is Empty</h2>";
+totalPrice.innerText = "0";
+return;
+}
+
+cart.forEach((product,index)=>{
+
+let price = parseInt(product.price.replace("₹",""));
+
+total += price;
+
+cartItems.innerHTML += `
+<div class="item">
+
+<img src="${product.image}">
+
+<div class="item-details">
+<h3>${product.name}</h3>
+<p>${product.price}</p>
+</div>
+
+<button class="remove-btn"
+onclick="removeItem(${index})">
+Remove
+</button>
+
+</div>
+`;
+
+});
+
+totalPrice.innerText = total;
+
+}
+
+function removeItem(index){
+
+cart.splice(index,1);
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+displayCart();
+
+}
+
+displayCart();
+
+document.getElementById("orderBtn")
+.addEventListener("click",()=>{
+
+alert("Order Placed Successfully!");
+
+localStorage.removeItem("cart");
+
+window.location.reload();
+
+});
